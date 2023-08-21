9c2f084f0ef48e41a1a76e7c25382400384c111e
euphoriiah-dev <euphoriiah.dev@gmail.com>
2023-08-21 19:27:53 +0200

    modifications from Surface pro

---
+++ src/Components/Introduction.jsx
@@ -0,0 +1,27 @@
+// React Imports
+import React from "react";
+
+// Style Imports
+import '../Components/style/introduction.css'
+
+function Introduction () {
+    return (
+        <section className="introduction-container">
+            <div className="introSocials-container">
+
+            </div>
+            <div className="introduction-part">
+                <div className="pp-container">
+
+                </div>
+                <div className="text-container">
+                    <h1>Malory Begue</h1>
+                    <h2> Développeuse web front-end & future concepteur designer UI </h2>
+                    <p></p>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Introduction
