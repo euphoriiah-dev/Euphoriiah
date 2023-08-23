fae5d170f260e039948a219ce63012b734dd285a
euphoriiah-dev <euphoriiah.dev@gmail.com>
2023-08-23 20:33:32 +0200

    dl button with text introduction adjustment

--- src/Components/Introduction.jsx
+++ src/Components/Introduction.jsx
@@ -4,8 +4,13 @@ import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope} from '@fortawesome/free-solid-svg-icons';
 import { faXTwitter, faLinkedinIn, faInstagram} from '@fortawesome/free-brands-svg-icons';
+// IMG Imports
+import introPicture from '../Assets/Img/profile_picture_eupho.webp';
 // Style Imports
-import '../Components/style/introduction.css'
+import '../Components/style/introduction.css';
+import monCV from '../Assets/Documents/CV-Malory_Begue_1.pdf'
+
+
 
 function Introduction () {
     return (
@@ -36,13 +41,32 @@ function Introduction () {
             </div>
             <div className="introduction-part">
                 <div className="pp-container">
-
+                    <img src={introPicture} alt="euphoriiah profile picture" />
                 </div>
                 <div className="text-container">
                     <h1>Malory Begue</h1>
                     <h2> Développeuse web front-end & future concepteur designer UI </h2>
-                    <p></p>
+                    <p>
+                    Salut ! Moi, c'est Malory, une aventurière digitale naviguant dans l'univers 
+                    du développement web. Je pourrais vous raconter que j'ai toujours su que je finirais par taper
+                    des lignes de code, mais ça ne s'est pas passé exactement comme ça ! J'ai traversé l'univers
+                    impitoyable du commerce, avant de finalement trouver ma voie dans le développement web, le web design et la conception d'interfaces !
+                    <br/><br/> Je vous invite à parcourir ma page pour explorer mes créations, travaux et même ma galerie photo. Alors,
+                    attachez vos ceintures et préparez-vous à plonger dans mon monde numérique. 
+                    <br/><br/>Bienvenue dans mon coin de la toile !
+                    </p>
                 </div>
+            <a href={monCV} download>    
+                <div className="cv-download">
+                    <div className="dl-button">
+                        <div class="downloading">
+                            <span class="custom-arrow"></span>
+                        </div>
+                        <div class="bar"></div>
+                    </div>
+                    <p> Télécharge mon CV </p>
+                </div>
+            </a>
             </div>
         </section>
     )
