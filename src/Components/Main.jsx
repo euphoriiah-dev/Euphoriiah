993a8ab1d2cc649c1f5a8d8606def67ccdcdb263
euphoriiah-dev <euphoriiah.dev@gmail.com>
2023-08-25 23:51:16 +0200

    adjustments on mobile

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -11,29 +11,34 @@ function Main(){
   return(
     <div className='main-container parallax-wrapper'>
         <section className='first-section'>
-          <div class="titles">
+          <div className="titles">
             <hgroup>
               <h1>Vous recherchez une alternante ?</h1>
               <h2>En conception et design UI / UX, webdesign ou Chargée de communication ?</h2> 
             </hgroup>
           </div>
         </section>
-        <section>
-          <div class="p-one parallax-inner">
+        <section className='section-2'>
+          <div className="p-one parallax-inner">
             <h2>Parfait ! Moi je recherche un poste ! </h2>
           </div>
         </section>
-        <section>
-          <div class="content-text">
-            <p><span class="first-letter red">J</span>
+        <section className='section-1'>
+          <div className="content-text">
+            <p className='first-p'><span class="first-letter red">J</span>
       e recherche un poste en alternance pour la rentrée 2023, je suis disponible immédiatement.
       Je suis mobile sur Clermont-Ferrand, Vichy, Issoire, Riom, Roanne et sur Lyon ! 
       Comme je l'ai spécifié un peu plus haut, je recherche un poste en conception et design UI, en UX,
       en webdesign mais éventuellement en communication digitale. </p>
             <div class="line-break"></div>
             <p></p>
-    </div>
-  </section>
+          </div>
+        </section>
+        <section className='section-4'>
+          <div class="p-two parallax-inner">
+            <h2>Contactez-moi !</h2>
+          </div>
+      </section>
     </div>
   )
 }
